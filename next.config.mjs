087cfb37949736701b084848a455893fb2987349
kunika <kunikak@gmail.com>
2024-10-13 11:20:45 +0100

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/data-register-app",
+    output: "export",
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            sharp$: false,
+            'onnxruntime-node$': false,
+        };
+        return config;
+    },
+};
+
+export default nextConfig;
